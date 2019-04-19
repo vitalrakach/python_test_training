a31c8ddfe3a4945d40015252642c3a81b0594c48
vitalrakach <vitalrakach@gmail.com>
2019-04-19 14:16:19 +0300

    Выжедид кдасс-плмошник ContactHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,7 +12,7 @@ def app(request):
     return fixture
 
 
-def test_add_user(app):
+def test_add_contact(app):
         app.session.login(username="admin", password="secret")
         app.create_contact(New_contact(firstname="Vital", middlename="Leonidovich", lastname="Rakach", nickname="v1t_al", title="ZAO", company="Adani", address="Minsk_work", home="1234567",
                                        mobile="9876543", work="4394423", fax="3487654", email="rakach1@mail.com", email2="rakach2@mail.com", email3="rakach3@mail.com",
