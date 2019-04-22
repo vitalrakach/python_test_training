d6146726668ba3e530f6f51202f7093bc066ad85
vitalrakach <vitalrakach@gmail.com>
2019-04-22 18:58:30 +0300

    Использование конструкции IF при изменении полей в тесте редактирования первой группы.
    3 теста, каждый изменяет только одно поле(name, header, footer).
    
    В fixture\group выделены вспомогательные методы для исключения дублирования кода вспомогательных действий
    edit_first_group
    fill_group_form
    change_field_value (здесь осуществляется проверка заполнения поля)

--- fixture/group.py
+++ fixture/group.py
@@ -13,15 +13,7 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -40,25 +32,32 @@ class GroupHelper:
     def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element_by_link_text("groups").click()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # summit deletion
+        self.select_first_group()
+        # open editing form
         wd.find_element_by_name("edit").click()
-        # insert values into fields
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        # insert values into field group_name
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def return_to_groups_page(self):
         wd = self.app.wd
