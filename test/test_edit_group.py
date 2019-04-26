00691bdd33c11f1ff1652d7c6bc4a7ad896e87e9
vitalrakach <vitalrakach@gmail.com>
2019-04-26 16:47:09 +0300

    Добавил предусловия выполнения тестов для следующих тестов: test_del_group test_edit_group
    Если при запуске теста в списке групп таковые отсутствует, то будет добавлена группа.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,12 +3,18 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="EDIT"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(header="edit_group_HEADER"))
 
 
 def test_edit_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(footer="edit_group_FOOTER"))
