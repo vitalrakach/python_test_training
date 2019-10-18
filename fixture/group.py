557614a531609ce4809dedf55f4a1eade4aed23d
vitalrakach <vitalrakach@gmail.com>
2019-10-18 11:13:15 +0300

    Задание 13. Добавил тест удаления рандомной группы. В fixture.group добавлены вспомогательные методы выбора рандомной группы и удаления рандомной группы, также модифицирован вспомогательный метод удаления первой группы (теперь в него передаем индекс 0).

--- fixture/group.py
+++ fixture/group.py
@@ -44,10 +44,17 @@ class GroupHelper:
 
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+
+    def select_group_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
         # summit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
